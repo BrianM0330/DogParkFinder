d58c1a57c91cf3a2973e3ce020d15ce1bfe92f46
BrianM0330 <BrianM0330@gmail.com>
2021-05-25 17:03:30 -0500

    better mock data

--- src/app/find-and-filter/park-data.ts
+++ src/app/find-and-filter/park-data.ts
@@ -1,40 +1,43 @@
 import {FilterOptions} from './filter.model';
 
+export interface ParkObject { //define the  necessary attributes of a park object
+  name: string;
+  options: FilterOptions[];
+  latitude: number;
+  longitude: number;
+}
+
 export class Parks {
-  parksList: Object = {}
+  public parksList: ParkObject[] = [
+    {
+      name : "Small Dog Park",
+      options : [FilterOptions.SMALL_DOGS, FilterOptions.LOW_CAPACITY, FilterOptions.LOW_POPULATION, FilterOptions.PUBLIC],
+      latitude : 69.420,
+      longitude : 1337
+    },
+
+    {
+      name : "Big Boi Park",
+      options : [FilterOptions.BIG_DOGS, FilterOptions.HIGH_CAPACITY, FilterOptions.HIGH_POPULATION, FilterOptions.PUBLIC],
+      latitude : 69.420,
+      longitude : 1337
+    },
+
+    {
+      name : "Rich People Only",
+      options : [FilterOptions.SMALL_DOGS, FilterOptions.BIG_DOGS, FilterOptions.LOW_CAPACITY, FilterOptions.LOW_POPULATION, FilterOptions.PRIVATE, FilterOptions.MEMBERSHIP],
+      latitude : 69.420,
+      longitude : 1337
+    },
+
+    {
+      name : "Fido's home",
+      options : [FilterOptions.SMALL_DOGS, FilterOptions.LOW_CAPACITY, FilterOptions.LOW_POPULATION, FilterOptions.PUBLIC],
+      latitude : 69.420,
+      longitude : 1337
+    }
+  ]
 
   constructor () {
-    this.parksList =
-    {
-      parks: [
-        {
-          name : "Small Dog Park",
-          options : [FilterOptions.SMALL_DOGS, FilterOptions.LOW_CAPACITY, FilterOptions.LOW_POPULATION, FilterOptions.PUBLIC],
-          latitude : 69.420,
-          longitude : 1337
-        },
-
-        {
-          name : "Big Boi Park",
-          options : [FilterOptions.BIG_DOGS, FilterOptions.HIGH_CAPACITY, FilterOptions.HIGH_POPULATION, FilterOptions.PUBLIC],
-          latitude : 69.420,
-          longitude : 1337
-        },
-
-        {
-          name : "Rich People Only",
-          options : [FilterOptions.SMALL_DOGS, FilterOptions.BIG_DOGS, FilterOptions.LOW_CAPACITY, FilterOptions.LOW_POPULATION, FilterOptions.PRIVATE, FilterOptions.MEMBERSHIP],
-          latitude : 69.420,
-          longitude : 1337
-        },
-
-        {
-          name : "Fido's home",
-          options : [FilterOptions.SMALL_DOGS, FilterOptions.LOW_CAPACITY, FilterOptions.LOW_POPULATION, FilterOptions.PUBLIC],
-          latitude : 69.420,
-          longitude : 1337
-        }
-      ]
-    }
   }
 }
