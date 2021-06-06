2b56776da06e998c11bc96fc7b64c92a13ea10fb
BrianM0330 <BrianM0330@gmail.com>
2021-06-06 14:16:21 -0500

    fixed project name conflicts

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,8 +10,10 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('dog-park-finder app is running!');
+    expect(page.getTitleText()).toEqual('DogParkFinder app is running!');
   });
+
+  afterEach(async () => {
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
