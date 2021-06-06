762f3607729eec9ddb3770b11d903a8f6b04a59f
BrianM0330 <BrianM0330@gmail.com>
2021-06-06 15:33:10 -0500

    find page to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ReviewBoardComponent } from './review-board/review-board.component';
+import { FindAndFilterComponent } from './find-and-filter/find-and-filter.component'
 
 const routes: Routes =
 [
   { path: 'home', component: HomeComponent},
-  { path: 'reviews', component: ReviewBoardComponent}
+  { path: 'reviews', component: ReviewBoardComponent},
+  { path: 'find', component: FindAndFilterComponent}
 ];
 
 @NgModule({
